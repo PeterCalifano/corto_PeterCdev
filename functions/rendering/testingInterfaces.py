daa2da112322ed74f3bd09bddd757ed9ff6b25ae
PeterC <petercalifano.gs@gmail.com>
2024-02-26 23:43:04 +0100

    Testing of JSON interface prototype completed

--- functions/rendering/testingInterfaces.py
+++ functions/rendering/testingInterfaces.py
@@ -60,14 +60,56 @@ def read_parse_configJSON(configJSONfilePath):
     geometry = {}
     scene = {}
     corto = {}
+    scenarioData = {}
+
     # Load JSON file parsing numbers
     with open(configJSONfilePath, 'r') as json_file:
-        ConfigDataJSON = json.load(json_file, parse_float=float, parse_int=int)
-    # Test printing
-    print(ConfigDataJSON)
+        ConfigDataJSONdict = json.load(json_file) # Load JSON as dict
 
+    if isinstance(ConfigDataJSONdict, dict):
+        # Get JSONdict scenarioData
+        CameraData = ConfigDataJSONdict['CameraData']
+        BlenderOpts = ConfigDataJSONdict['BlenderOpts']
+        SceneData = ConfigDataJSONdict['SceneData']
 
-    # Display loaded variables
+    elif isinstance(ConfigDataJSONdict, list):
+        # Pretty Printing has been enabled in MATLAB I guess...
+        raise Exception('Decoded JSON as list not yet handled by this implementation. If JSON comes from MATLAB jsonencode(), disable PrettyPrint option.')
+
+    # Manual Mapping to current CORTO version (26 Feb 2024)
+    # SCENE
+    scene['fov'] =  CameraData['fov'  ]
+    scene['resx'] = CameraData['resx' ]
+    scene['resy'] = CameraData['resy' ]
+    scene['labelDepth']  = SceneData['labelDepth'] 
+    scene['labelID']  = SceneData['labelID'] 
+    scene['labelSlopes']  = SceneData['labelSlopes'] 
+
+    # BODY
+    body['name'] = SceneData['scenarioName']
+    body['num'] = 1
+
+    # BLENDER OPTIONS
+    scene['encoding'] = BlenderOpts['encoding']
+    scene['rendSamples'] = BlenderOpts['rendSamples']
+    scene['viewSamples'] = BlenderOpts['viewSamples']
+    scene['scattering'] = BlenderOpts['scattering']
+    scene['viewtransform'] = BlenderOpts['viewtransform']
+    scene['filmexposure'] = BlenderOpts['filmexposure']
+
+    corto['savepath'] = os.path.normpath(BlenderOpts['savepath'])
+
+    # CAMERA, TARGET and ILLUMINATION
+    scenarioData['rStateCam']    = np.array(SceneData['rStateCam'])    
+    scenarioData['rTargetBody']  = np.array(SceneData['rTargetBody'])
+    scenarioData['rSun']         = np.array(SceneData['rSun'])         
+    scenarioData['qFromCAMtoIN'] = np.array(SceneData['qFromCAMtoIN'])
+    scenarioData['qFromTFtoIN']  = np.array(SceneData['qFromTFtoIN'])  
+    scenarioData['ID'] = np.arange(len(SceneData['rStateCam'])).flatten()
+
+    geometry['ii0'] = 0 # Initial index for rendering
+
+    # Display loaded configuration variables
     for key, value in body.items():
         print(f"{key}: {value}")
     print('')
@@ -81,12 +123,23 @@ def read_parse_configJSON(configJSONfilePath):
         print(f"{key}: {value}")
     print('')
 
-    return body, geometry, scene, corto
+    return body, geometry, scene, corto, scenarioData
 
 if __name__ == '__main__':
     configJSONfilePath = os.path.abspath('C:\\devDir\\corto_PeterCdev\\functions\\rendering\\CORTO_CONFIG.json')
-    ConfigDataJSON = read_parse_configJSON(configJSONfilePath)
-
+    body, geometry, scene, corto, scenarioData = read_parse_configJSON(configJSONfilePath)
+    scale_BU = 1
+    # ID
+    ID_pose = scenarioData['ID']
+    # Body
+    R_pos_BODY = scenarioData['rTargetBody']*scale_BU # (BU) 
+    R_q_BODY = scenarioData['qFromTFtoIN'] #from_txt[:,4:8] # (-) 
+    # Camera
+    R_pos_SC = scenarioData['rStateCam']*scale_BU #from_txt[:,8:11]*scale_BU # (BU) 
+    R_q_SC = scenarioData['qFromCAMtoIN'] # from_txt[:,11:15] # (-) 
+    # Sun 
+    R_pos_SUN = scenarioData['rSun'] #from_txt[:,15:18] # (BU) 
+    
     #read_parse_configJSON(configJSONfilePath)
     print('EXECUTION TERMINATION LINE')
 
