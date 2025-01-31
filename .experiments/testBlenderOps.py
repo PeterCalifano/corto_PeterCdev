ca4817aa6b5821b73eea5f21690625b80f906401
PeterC <petercalifano.gs@gmail.com>
2025-01-31 19:39:27 +0100

    Add test method to load blender file (placeholder)

--- .experiments/testBlenderOps.py
+++ .experiments/testBlenderOps.py
@@ -3,4 +3,11 @@ def importBPY():
     print("Imported bpy correctly")
     # Get an example option from bpy
     print(bpy.context.preferences.addons.keys())
+
+
+def importBlendFile():
+    import bpy
     
+    bpy.ops.wm.open_mainfile(filepath="/home/lorenzo/Downloads/BlenderFiles/scene.blend")
+    print("Opened file correctly")
+
